f2dd6cf54d5e2b1f6fa24f9ab1a665712d293ee2
udaysarkarud <udaysarkarud@gmail.com>
2022-03-25 04:25:51 +0600

    added cloudinary, axios and spinner

--- src/components/AddNewPost/AddNewPost.js
+++ src/components/AddNewPost/AddNewPost.js
@@ -1,20 +1,43 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 
+
+
 const AddNewPost = () => {
+    const [loading, setLoading] = useState(false)
+
     const { register, handleSubmit, watch, formState: { errors }, reset } = useForm();
 
     const onSubmit = data => {
-        console.log(data)
+        setLoading(true)
+        let blogThumbnail;
+        let blogPostData;
+        const fileInfo = data.blogImg[0];
+        const formData = new FormData();
+        formData.append('file', fileInfo);
+        formData.append('upload_preset', 'plwdtmz7');
+
+        axios.post('https://api.cloudinary.com/v1_1/coremailud/image/upload', formData).then(res => {
+            blogThumbnail = `${res.data.url}`;
+            delete data['blogImg'];
+            blogPostData = { ...data, blogThumbnail }
+            console.log(blogPostData)
+            setLoading(false)
+        })
     };
     return (
         <section className="container section-divider">
             <div className="row gx-4 gx-lg-5">
                 <div className="col">
                     <div>
+
                         <h1>Add New Blog Post</h1>
+
+
                         <p>Add new usefull content to help people to choose the right show and how they'll maintain the product. </p>
                     </div>
+
                     <form className="row g-3" autoComplete="off" onSubmit={handleSubmit(onSubmit)}>
                         <div className="col-12">
                             <label className="form-label">Blog Title</label>
@@ -29,7 +52,7 @@ const AddNewPost = () => {
                         <div className="col-md-6">
                             <label className="form-label">Blog Thumbnail</label>
 
-                            <input type="file" {...register("blogThumbnail")}
+                            <input type="file" {...register("blogImg")}
                                 accept="image/png, image/gif, image/jpeg"
                                 className="form-control" placeholder="Image URL" required />
                         </div>
@@ -38,10 +61,21 @@ const AddNewPost = () => {
                             <textarea {...register("blogDescription")} className="form-control" placeholder="Description" rows="6" required />
                         </div>
 
-                        <div className="col-12">
+                       
+
+                        
+                        <div className="col-10">
                             <button type="submit" className="btn btn-primary">Add New Blog</button>
-                            
+
                             <button className="btn btn-dark btn-lg btn-block ms-2" type="reset">Clear fields</button>
+                        </div>
+                        <div className="col-2">
+                            {
+                                loading&&<div className="spinner-grow text-danger" style={{width: '3rem', height: '3rem'}} role="status">
+                                <span className="visually-hidden">Loading...</span>
+                            </div>
+                            }
+                            
                         </div>
 
                     </form>
