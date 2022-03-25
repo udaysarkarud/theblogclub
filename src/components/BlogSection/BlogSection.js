547eb3f5597bae6b6f8a162bf662da37121f1552
udaysarkarud <udaysarkarud@gmail.com>
2022-03-26 03:31:28 +0600

    added search option

--- src/components/BlogSection/BlogSection.js
+++ src/components/BlogSection/BlogSection.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import SingelBlogCard from '../SingelBlogCard/SingelBlogCard';
 import ReactPaginate from 'react-paginate';
+import { useForm } from 'react-hook-form';
 
 const BlogSection = () => {
     const [blogPosts, setBlogPosts] = useState([]);
@@ -24,6 +25,21 @@ const BlogSection = () => {
         setPage(data.selected);
     }
 
+    const { register, handleSubmit, watch, formState: { errors }, reset } = useForm();
+
+    const onSubmit = data => {
+        console.log(data.search)
+        if (data.search) {
+            axios.get(`http://localhost:5000/blogposts?page=${page}&&size=${size}&&search=${data.search}`)
+            .then(res => {
+                setBlogPosts(res.data.allPosts);
+                const count = res.data.count;
+                const pageNumber = Math.ceil(count / size);
+                setPageCount(pageNumber);
+            })
+        }
+    };
+
     return (
         <section className="container section-divider">
             <div className="row">
@@ -33,10 +49,13 @@ const BlogSection = () => {
                         <p>We have more than 1k authors. They provide nice and well researched contents for your. Our stories that move you and move with you</p>
                     </div>
 
-                    <div className="input-group my-5">
-                        <input type="text" className="form-control" placeholder="Search using blog title" aria-label="Recipient's username" aria-describedby="button-addon2" />
-                        <button className="btn btn-outline-secondary" type="button" id="button-addon2">Search</button>
-                    </div>
+                    <form className="row" autoComplete="off" onSubmit={handleSubmit(onSubmit)}>
+                        <div className="input-group my-5">
+                            <input type="text" className="form-control" placeholder="Search using blog title" {...register("search")} />
+                            <button className="btn btn-outline-secondary" type="submit" id="button-addon2">Search</button>
+                        </div>
+                    </form>
+
                 </div>
             </div>
             <div className="row">
